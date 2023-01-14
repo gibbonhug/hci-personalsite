e2f6f3730f229a2b4428c05c8782951c75c1dc2c
Julian <juliangwillis@gmail.com>
2023-01-14 18:09:21 -0500

    Successfully implement passing JSX element as props to JournalEntries

--- src/components/Journal.tsx
+++ src/components/Journal.tsx
@@ -3,10 +3,12 @@ import Prev from "./Prev";
 import Next from "./Next";
 
 export default function Journal() {
+    const text = <p>I'm a JSX element</p>
+
     return (
         <div id="journal">
-            <JournalEntry id={"journal-entry-left"}></JournalEntry>
-            <JournalEntry id={"journal-entry-right"}></JournalEntry>
+            <JournalEntry id={"journal-entry-left"} text={text}></JournalEntry>
+            <JournalEntry id={"journal-entry-right"} text={text}></JournalEntry>
             <Prev></Prev>
             <Next></Next>
         </div>
--- src/components/JournalEntry.tsx
+++ src/components/JournalEntry.tsx
@@ -1,13 +1,12 @@
 interface JournalEntryProps {
     id: string;
+    text: JSX.Element;
 }
 
 export default function JournalEntry(props: JournalEntryProps) {
     return (
         <div className="journal-entry" id={props.id}>
-            <p>
-               I'm a little paragraph
-            </p>
+            {props.text}
         </div>
     )
 }
