3f22c006e5ab5ac828749b8ce06c6f2657624ab5
Julian <juliangwillis@gmail.com>
2023-01-14 16:24:02 -0500

    Add semicolons

--- src/components/Prev.tsx
+++ src/components/Prev.tsx
@@ -1,10 +1,10 @@
 import audio from '../assets/turn.mp3';
 
 export default function Prev() {
-    const aud_obj = new Audio(audio)
+    const aud_obj = new Audio(audio);
 
     const play = () => {
-        aud_obj.play()
+        aud_obj.play();
     }
 
     return (
