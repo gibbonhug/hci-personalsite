5efa8494d5754a6fcdd95ce25ed6c494a4a46816
Julian <juliangwillis@gmail.com>
2023-01-14 18:20:20 -0500

    add missing parenthesis

--- src/components/Journal.tsx
+++ src/components/Journal.tsx
@@ -4,7 +4,7 @@ import Next from "./Next";
 
 export default function Journal() {
     const fargoth = {
-        title: "16 Last Seed (Day 1",
+        title: "16 Last Seed (Day 1)",
         body: <p>
         I have met a Bosmer named <span>Fargoth</span> who claims to <span>Imperial</span>s have stolen a <span>ring</span> of his. He would like it back.
         </p>
