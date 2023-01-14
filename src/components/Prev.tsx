17439f8f7354aac42d8909f10b98e187e173de60
Julian <juliangwillis@gmail.com>
2023-01-14 16:28:27 -0500

    Add logic to stop and restart playback from beginning for turn page audio

--- src/components/Prev.tsx
+++ src/components/Prev.tsx
@@ -4,6 +4,10 @@ export default function Prev() {
     const turnAudioObject = new Audio(turnAudio);
 
     const playAudio = () => {
+        // if it is already playing sound, stop the sound
+        turnAudioObject.pause();
+        turnAudioObject.currentTime = 0;
+        // play
         turnAudioObject.play();
     }
 
