99567a7b24834001d772a60d0c9f9bb1e39b62a6
Julian <juliangwillis@gmail.com>
2023-01-14 18:11:22 -0500

    Add Fargoth dummy text

--- src/components/Journal.tsx
+++ src/components/Journal.tsx
@@ -5,9 +5,15 @@ import Next from "./Next";
 export default function Journal() {
     const text = <p>I'm a JSX element</p>
 
+    const textFargoth = (
+        <p>
+            I have met a Bosmer named <span>Fargoth</span> who claims to <span>Imperial</span>s have stolen a <span>ring</span> of his. He would like it back.
+        </p>
+    );
+
     return (
         <div id="journal">
-            <JournalEntry id={"journal-entry-left"} text={text}></JournalEntry>
+            <JournalEntry id={"journal-entry-left"} text={textFargoth}></JournalEntry>
             <JournalEntry id={"journal-entry-right"} text={text}></JournalEntry>
             <Prev></Prev>
             <Next></Next>
