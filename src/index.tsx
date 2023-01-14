8430d81799f230dd1735e2b41716d0936c0e5e50
Julian <juliangwillis@gmail.com>
2023-01-14 14:43:12 -0500

    Add new react_components directory; dummy styling for new Journal component

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './style.css';
+import Journal from './components/Journal';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -9,5 +10,6 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     Hello, Human Computer Interaction
+    <Journal></Journal>
   </React.StrictMode>
 );
