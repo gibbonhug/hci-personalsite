82aa7abbc65e962c0d4da428672ed96c43e34b6e
Julian <juliangwillis@gmail.com>
2023-01-15 16:24:58 -0500

    Reorganize code in App component

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -4,17 +4,12 @@ import titleAudioImport from '../assets/title.mp3';
 import { useEffect, useState } from 'react';
 
 export default function App() {
-    // Open journal with 'j' key event listener; for conditional rendering
-
+    /*
+        JOURNAL OPENING/CLOSING
+    */
     const [isJournalOpen, setIsJournalOpen] = useState(false);
 
-    const titleAudio = new Audio(titleAudioImport);
-
-    // Play audio on render (note: will only work if user allows autoplay)
-    useEffect(() => {
-        titleAudio.play();
-    }, []);
-
+    // Open journal with 'j' key event listener; for conditional rendering
     useEffect(() => {
         const handleKeyPress = (event: KeyboardEvent) => {
             if (event.key === 'j' || event.key === 'J') {
@@ -30,11 +25,22 @@ export default function App() {
 
     }, [isJournalOpen]);
 
+    /*
+        AUDIO
+    */
+
+    const titleAudio = new Audio(titleAudioImport);
+
     // Loop audio
     titleAudio.addEventListener('ended', () => {
         titleAudio.currentTime = 0;
         titleAudio.play();
     });
+
+    // Play audio on render (note: will only work if user allows autoplay)
+    useEffect(() => {
+        titleAudio.play();
+    }, []);
 
     return (
         <div id="app-wrapper">
