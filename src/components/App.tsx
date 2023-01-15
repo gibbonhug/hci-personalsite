39bffefa297f007acc1bc39a209b8e1f15f58092
Julian <juliangwillis@gmail.com>
2023-01-14 23:12:22 -0500

    Add autoplay and loop to title theme in App component

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,11 +1,21 @@
 import Journal from "./Journal";
 
 import titleAudioImport from '../assets/title.mp3';
+import { useEffect } from "react";
 
 export default function App() {
     const titleAudio = new Audio(titleAudioImport);
 
-    titleAudio.play(); // need to add logic to loop
+    // Play audio on render (note: will only work if user allows autoplay)
+    useEffect(() => {
+        titleAudio.play(); // need to add logic to loop
+    }, []);
+
+    // Loop audio
+    titleAudio.addEventListener('ended', () => {
+        titleAudio.currentTime = 0;
+        titleAudio.play();
+    })
     
     return (
         <div id="app-wrapper">
