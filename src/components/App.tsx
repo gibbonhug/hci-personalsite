4491facc4582cc487579ae4215c4a32e7bbf272d
Julian <juliangwillis@gmail.com>
2023-01-15 16:23:42 -0500

    Create state and useEffect listener to open the Journal component with J key; temp text with instrucs

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,9 +1,13 @@
-import Journal from "./Journal";
+import Journal from './Journal';
 
 import titleAudioImport from '../assets/title.mp3';
-import { useEffect } from "react";
+import { useEffect, useState } from 'react';
 
 export default function App() {
+    // Open journal with 'j' key event listener; for conditional rendering
+
+    const [isJournalOpen, setIsJournalOpen] = useState(false);
+
     const titleAudio = new Audio(titleAudioImport);
 
     // Play audio on render (note: will only work if user allows autoplay)
@@ -11,15 +15,31 @@ export default function App() {
         titleAudio.play();
     }, []);
 
+    useEffect(() => {
+        const handleKeyPress = (event: KeyboardEvent) => {
+            if (event.key === 'j' || event.key === 'J') {
+                setIsJournalOpen((isJournalOpen) => (!isJournalOpen))
+            }
+        };
+
+        document.addEventListener('keydown', handleKeyPress);
+
+        return(() => {
+            document.removeEventListener('keydown', handleKeyPress)
+        })
+
+    }, [isJournalOpen]);
+
     // Loop audio
     titleAudio.addEventListener('ended', () => {
         titleAudio.currentTime = 0;
         titleAudio.play();
-    })
-    
+    });
+
     return (
         <div id="app-wrapper">
-            <Journal></Journal>
+            <div className='temp'>Open Journal with the J key</div>
+            {isJournalOpen && <Journal></Journal>}
         </div>
-    )
+    );
 }
