9133437c6b0f6fd946265070c6d3ec3fb369c27d
Julian <juliangwillis@gmail.com>
2023-01-14 19:45:04 -0500

    Add title music

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,6 +1,12 @@
 import Journal from "./Journal";
 
+import titleAudioImport from '../assets/title.mp3';
+
 export default function App() {
+    const titleAudio = new Audio(titleAudioImport);
+
+    titleAudio.play();
+    
     return (
         <div id="app-wrapper">
             <Journal></Journal>
