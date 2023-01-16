4cefd2bf7abea107ee3e8f7e35dae77c49cba19b
Julian <juliangwillis@gmail.com>
2023-01-15 21:19:01 -0500

    Move old skills stuff into sections const; overflow scroll

--- src/components/Inventory.tsx
+++ src/components/Inventory.tsx
@@ -4,46 +4,44 @@ import gsuMapAsset from '../assets/gsumap.jpg';
 interface InventoryProps {}
 
 export default function Inventory(props: InventoryProps) {
-    const stuff = (
-        <>
-            <div>
-                Major Skills (Course-Related)
-                <ul>
-                    <li>React.js</li>
-                    <li>CSS</li>
-                    <li>Research</li>
-                    <li>Technical documentation</li>
-                </ul>
-            </div>
-            <div>
-                Minor Skills (Course-Related)
-                <ul>
-                    <li>Graphic Design</li>
-                    <li>Typography</li>
-                </ul>
-            </div>
-            <div>
-                Misc Skills (Hobbies/Interests)
-                <ul>
-                    <li>Maskmaking (Venetian masks)</li>
-                    <li>Painting</li>
-                    <li>Baking</li>
-                    <li>Loose leaf tea</li>
-                    <li>Tabletop games</li>
-                    <li>Roguelike games</li>
-                    <li>Philosophy</li>
-                    <li>Sudoku</li>
-                    <li>Algorithms</li>
-                    <li>Running</li>
-                </ul>
-            </div>
-        </>
-    );
-
     const sections = [
         {
             title: 'Julian Willis',
-            body: <></>,
+            body: (
+                <>
+                    <div>
+                        Major Skills (Course-Related)
+                        <ul>
+                            <li>React.js</li>
+                            <li>CSS</li>
+                            <li>Research</li>
+                            <li>Technical documentation</li>
+                        </ul>
+                    </div>
+                    <div>
+                        Minor Skills (Course-Related)
+                        <ul>
+                            <li>Graphic Design</li>
+                            <li>Typography</li>
+                        </ul>
+                    </div>
+                    <div>
+                        Misc Skills (Hobbies/Interests)
+                        <ul>
+                            <li>Maskmaking (Venetian masks)</li>
+                            <li>Painting</li>
+                            <li>Baking</li>
+                            <li>Loose leaf tea</li>
+                            <li>Tabletop games</li>
+                            <li>Roguelike games</li>
+                            <li>Philosophy</li>
+                            <li>Sudoku</li>
+                            <li>Algorithms</li>
+                            <li>Running</li>
+                        </ul>
+                    </div>
+                </>
+            ),
         },
         {
             title: 'Aderhold Learning Center',
